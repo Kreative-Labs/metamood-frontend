de069733b7e2b7f71f211bfbeb73e4dbf7bc55d3
Rakshit <rakshit087@gmail.com>
2022-04-06 02:38:04 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#392C5D",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <h1 style={{ color: "white", fontFamily:"'Press Start 2P'", padding:"32px 0" }}>Meta Mood</h1>
+    </div>
+  );
+}
+
+export default App;
