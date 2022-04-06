f12db9752c480c85f97b8b10d638c18b369506d7
Rakshit <rakshit087@gmail.com>
2022-04-06 13:01:38 +0530

    Converted Data to State ⚛

--- src/components/DataVis.jsx
+++ src/components/DataVis.jsx
@@ -1,31 +1,31 @@
+import { useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, Cell } from "recharts";
 
-const data = [
-  {
-    name: "Valence",
-    value: 0.8,   
-  },
-  {
-    name: "Arousal",
-    value: 0.6,
-  },
-  {
-    name: "Dominance",
-    value: 0.8,
-  },
-];
-
-const colors = ["#FF10F0", "#00DAFF", "#39FF14"]
+const colors = ["#FF10F0", "#00DAFF", "#39FF14"];
 
 export const DataVis = () => {
+  const [data, setData] = useState([
+    {
+      name: "Valence",
+      value: 0.8,
+    },
+    {
+      name: "Arousal",
+      value: 0.6,
+    },
+    {
+      name: "Dominance",
+      value: 0.8,
+    },
+  ]);
   return (
     <ResponsiveContainer width="30%" height="50%">
       <BarChart width={150} height={40} data={data}>
         <XAxis dataKey="name" stroke="#f2f2f2" />
         <Bar dataKey="value">
-        <Cell key={1} fill={colors[0]} />
-        <Cell key={2} fill={colors[1]} />
-        <Cell key={3} fill={colors[2]} />
+          <Cell key={1} fill={colors[0]} />
+          <Cell key={2} fill={colors[1]} />
+          <Cell key={3} fill={colors[2]} />
         </Bar>
       </BarChart>
     </ResponsiveContainer>
