427943bdc73a927591c6fded1709bf3e78ed3af4
Rakshit <rakshit087@gmail.com>
2022-04-06 12:56:26 +0530

    Created Charts

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { DataVis } from "./components/DataVis";
 import { Space } from "./components/Space";
 
 function App() {
@@ -8,11 +9,14 @@ function App() {
         width: "100vw",
         backgroundColor: "#392C5D",
         display: "flex",
-        justifyContent: "center",
+        flexDirection:"column",
+        alignItems: "center",
       }}
     >
       <Space />
-      <h1 style={{ color: "white", fontFamily:"'Press Start 2P'", padding:"32px 0" }}>Meta Mood</h1>
+      <h1 style={{ color: "white", fontFamily:"'Press Start 2P'", padding:"32px 0", marginBottom:"64px"}}>Meta Mood</h1>
+      <DataVis />
+      <h3 style={{ color: "white", fontFamily:"'Press Start 2P'"}}>HAPPY</h3>
     </div>
   );
 }
---
+++ src/components/DataVis.jsx
@@ -0,0 +1,33 @@
+import { BarChart, Bar, ResponsiveContainer, XAxis, Cell } from "recharts";
+
+const data = [
+  {
+    name: "Valence",
+    value: 0.8,   
+  },
+  {
+    name: "Arousal",
+    value: 0.6,
+  },
+  {
+    name: "Dominance",
+    value: 0.8,
+  },
+];
+
+const colors = ["#FF10F0", "#00DAFF", "#39FF14"]
+
+export const DataVis = () => {
+  return (
+    <ResponsiveContainer width="30%" height="50%">
+      <BarChart width={150} height={40} data={data}>
+        <XAxis dataKey="name" stroke="#f2f2f2" />
+        <Bar dataKey="value">
+        <Cell key={1} fill={colors[0]} />
+        <Cell key={2} fill={colors[1]} />
+        <Cell key={3} fill={colors[2]} />
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
