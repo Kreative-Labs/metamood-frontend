5968f9caa5640b624ebd8d717efb229d6c3764f8
Rakshit <rakshit087@gmail.com>
2022-04-10 17:15:44 +0530

    Getting Data from Backend 🥳

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,45 @@
 import { DataVis } from "./components/DataVis";
 import { Space } from "./components/Space";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([
+    {
+      name: "Valence",
+      value: 0.5,
+    },
+    {
+      name: "Arousal",
+      value: 0.5,
+    },
+    {
+      name: "Dominance",
+      value: 0.5,
+    },
+  ]);
+  const [mood, setMood] = useState("Neutral");
+  useEffect(() => {
+    const getData = async () => {
+      const moodData = await axios.get("http://127.0.0.1:5000/mood");
+      setData([
+        {
+          name: "Valence",
+          value: JSON.parse(moodData.data.data)[0],
+        },
+        {
+          name: "Arousal",
+          value: JSON.parse(moodData.data.data)[2],
+        },
+        {
+          name: "Dominance",
+          value: JSON.parse(moodData.data.data)[1],
+        },
+      ]);
+      setMood(moodData.data.mood)
+    };
+    getData();
+  }, []);
   return (
     <div
       style={{
@@ -9,14 +47,23 @@ function App() {
         width: "100vw",
         backgroundColor: "#392C5D",
         display: "flex",
-        flexDirection:"column",
+        flexDirection: "column",
         alignItems: "center",
       }}
     >
       <Space />
-      <h1 style={{ color: "white", fontFamily:"'Press Start 2P'", padding:"32px 0", marginBottom:"64px"}}>Meta Mood</h1>
-      <DataVis />
-      <h3 style={{ color: "white", fontFamily:"'Press Start 2P'"}}>HAPPY</h3>
+      <h1
+        style={{
+          color: "white",
+          fontFamily: "'Press Start 2P'",
+          padding: "32px 0",
+          marginBottom: "64px",
+        }}
+      >
+        Meta Mood
+      </h1>
+      <DataVis data={data} />
+      <h3 style={{ color: "white", fontFamily: "'Press Start 2P'" }}>{mood}</h3>
     </div>
   );
 }
--- src/components/DataVis.jsx
+++ src/components/DataVis.jsx
@@ -1,26 +1,12 @@
-import { useState } from "react";
 import { BarChart, Bar, ResponsiveContainer, XAxis, Cell } from "recharts";
 
 const colors = ["#FF10F0", "#00DAFF", "#39FF14"];
 
-export const DataVis = () => {
-  const [data, setData] = useState([
-    {
-      name: "Valence",
-      value: 0.8,
-    },
-    {
-      name: "Arousal",
-      value: 0.6,
-    },
-    {
-      name: "Dominance",
-      value: 0.8,
-    },
-  ]);
+export const DataVis = (props) => {
+
   return (
     <ResponsiveContainer width="30%" height="50%">
-      <BarChart width={150} height={40} data={data}>
+      <BarChart width={150} height={40} data={props.data}>
         <XAxis dataKey="name" stroke="#f2f2f2" />
         <Bar dataKey="value">
           <Cell key={1} fill={colors[0]} />
